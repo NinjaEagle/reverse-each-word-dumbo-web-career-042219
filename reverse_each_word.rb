f8e22ca9d3a0fa9e4045a539cfb03638fe31af79
Kevin W <kcwang06@gmail.com>
2019-04-09 23:31:39 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word (str)
+  if array.length == 1
+   array.join
+ elsif array.length == 2
+   array.join(" and ")
+ else
+   first_pt = array[0...-1]
+   second_pt = ", and " + array[-1]
+   first_pt.join(", ") + second_pt
+ end
